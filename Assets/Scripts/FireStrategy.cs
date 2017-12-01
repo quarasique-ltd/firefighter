7082b106b0d285cc7810aed97d849ad697843bab
AweMath <dart.fk712@gmail.com>
2017-12-02 03:37:37 +0600

    world updater script added

---
+++ Assets/Scripts/FireStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGenerator : WorldUpdater
+{
+		
+	public override void UpdateWorld()
+	{
+		GameObject FirePlaces [];
+		FirePlaces = GameObject.FindGameObjectWithTag("firePlaces");
+	}
+}
---
+++ Assets/Scripts/WorldUpdater.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WorldUpdater : MonoBehaviour {
+	
+	// Update is called once per frame
+	public abstract void UpdateWorld();
+}
