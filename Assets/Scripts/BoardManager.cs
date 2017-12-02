406970e13c9a4631ed5b087bd4aa15aa3e01f050
AweMath <dart.fk712@gmail.com>
2017-12-02 08:36:47 +0600

    add game manager & board manager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+	public IWorldGenerator[] WorldGenerators;
+
+	public IWorldUpdater []WorldUpdaters;
+	
+	// Use this for initialization
+	public void BoardSetup (int level) {
+		WorldGenerators = GetComponents<IWorldGenerator>();
+		WorldUpdaters = GetComponents<IWorldUpdater>();
+		foreach (IWorldGenerator worldGenerator in WorldGenerators)
+		{
+			worldGenerator.Init();
+		}
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		foreach (IWorldUpdater worldUpdater in WorldUpdaters)
+		{
+			worldUpdater.UpdateWorld();
+		}	
+	}
+}
